893ea493cfb4bd8fe394fc198200f18121f04241
adanahmed002 <adanahmed002@gmail.com>
2023-07-02 07:43:15 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #!/bin/bash
 #include <stdio.h>
 /**
- * 
  * prints "Programming is like building a multilingual puzzle
  * returns (0)
  */
 int main(void)
 {
-puts("/"Programming is like building a multilingual puzzle"/n)
+puts('"Programming is like building a multilingual puzzle');
 	return(0);
 }
