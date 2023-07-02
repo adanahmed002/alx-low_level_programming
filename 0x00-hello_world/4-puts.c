2d8e30e3b14e1ce1cf5ffcd40ed2cc3b9f6d70a7
adanahmed002 <adanahmed002@gmail.com>
2023-07-02 08:31:59 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@
  * Return:0 (Sucess)
  */
 int main(void) {
-    puts(""Programming is like building a multilingual puzzle");
+    puts("\"Programming is like building a multilingual puzzle");
     return 0;
 }
 
